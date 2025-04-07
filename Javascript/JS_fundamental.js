23b9ae4073082bf1ed02c84fc26944694f44ad0d
Rajat Kumar Das <rajatkumardas454@gmail.com>
2025-04-07 11:45:32 +0000

    js funda

---
+++ Javascript/JS_fundamental.js
@@ -0,0 +1,25 @@
+// javascript comparison operators
+// i. eaquality operator
+// a.equality operator(==)
+// b.inequality operator(!=)
+// c.strict equality operator(===)
+// d.strict inequality operator(!==)
+
+// ii. Relational operator
+// a. greater than operator(>)
+// b. less than operator(<)
+// c. greater than or equal to operator(>=)
+// d. less than or equal to operator(<=)
+//
+// iii. Logical operator
+// a. logical AND operator(&&)
+// b. logical OR operator(||)
+// c. logical NOT operator(!)
+//
+// iv. Bitwise operator
+// a. bitwise AND operator(&)
+// b. bitwise OR operator(|)
+// c. bitwise XOR operator(^)
+// d. bitwise NOT operator(~)
+// e. left shift operator(<<)
+// f. right shift operator(>>)  
