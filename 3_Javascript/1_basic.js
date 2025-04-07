aeda2e8177da661a35dd03b82a57b6b548c3651c
Rajat Kumar Das <rajatkumardas454@gmail.com>
2025-04-07 10:36:19 +0000

    basic

---
+++ 3_Javascript/1_basic.js
@@ -0,0 +1,125 @@
+// // Data Types
+// // 1. Number
+// let num = 10;
+// console.log(num); // 10
+// // 2. String
+// let str = "Hello, World!";
+// console.log(str); // Hello, World!
+// // 3. Boolean
+// let bool = true;
+// console.log(bool); // true
+// // 4. Undefined
+// let und;
+// console.log(und); // undefined
+// // 5. Null
+// let nul = null;
+// console.log(nul); // null
+// // 6. Symbol
+// let sym = Symbol("unique");
+// console.log(sym); // Symbol(unique)
+// // 7. BigInt
+// let bigInt = BigInt(123456789012345678901234567890);
+// console.log(bigInt); // 123456789012345678901234567890n
+
+// // Non Primitive Data Types
+
+// // 1. Object
+// let obj = {
+//   name: "John",
+//   age: 30,
+//   city: "New York"
+// };
+// console.log(obj); // { name: 'John', age: 30, city: 'New York' }
+// //  another way to create an object
+// let obj2 = new Object();
+// obj2.name = "John";
+// obj2.age = 30;
+// obj2.city = "New York";
+// console.log(obj2); // { name: 'John', age: 30, city: 'New York' }
+
+// // 2. Array
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr); // [ 1, 2, 3, 4, 5 ]    
+// //  another way to create an array
+// let arr2 = new Array(1, 2, 3, 4, 5);
+// console.log(arr2); // [ 1, 2, 3, 4, 5 ]
+
+
+// 3. Function
+// function myFunction() {
+//   console.log("Hello, World!");
+// }
+// myFunction(); // Hello, World!
+// //  another way to create a function
+// let myFunction2 = function() {
+//   console.log("Hello, World!");
+// };
+// myFunction2(); // Hello, World!
+
+// aerow function
+// let myFunction3 = () => {
+//   console.log("Aerrow Function");
+// };
+// myFunction3(); // Hello, World!
+
+// 3. Date
+// let date = new Date();
+// console.log(date); // current date and time
+
+// 4. Object
+// let obj3 = new Object();
+// obj3.name = "John";
+// obj3.age = 30;
+// obj3.city = "New York";
+// console.log(obj3); // { name: 'John', age: 30, city: 'New York' }
+
+
+// // -----------------------------------------------------------------------
+// // concatenation
+// let str="Rajat"+" "+"Kumar";
+// console.log(str); // Rajat Kumar
+
+// // another way to concatenate
+// let str2="Rajat";
+// str2 +="";
+// str2 +="Kumar";
+// console.log(str2); // Rajat Kumar
+
+// // another way to concatenate
+// let str3="Values: ";
+// str3 += 10;
+// str3 += "";
+// str3 += {
+//   name: "Rajat",
+//   age: 25
+// };
+// str3 += ["mango", "apple "];
+// str3 += true;
+// str3 += null;
+// str3 += undefined;
+// console.log(str3); // Values: 10[object Object][object Object]truenullundefined
+
+// -----------------------------------------------------------------------------------------------------
+
+//------------------------------- Operators----------------------------------------------------------
+// 1. Arithmetic Operators
+// let a = 10;
+// let b = 5;
+// let c = a + b;
+// console.log(c); // 15
+
+// 2.subtraction
+// let a=10;
+// let b=5;
+// let c=a-b;
+// console.log(c);
+
+// 3. multiplication
+// let a=10;
+// let b=5;
+// let c=a*b;
+// console.log(c);//50
+
+// 4. division
+
+
---
+++ 3_Javascript/function.js
@@ -0,0 +1,9 @@
+// function myfun() {
+//     alert("Hello World");
+// }
+// myfun();
+// // another way to create a function
+// let myfun2 = function() {
+//     alert("Hello World");
+// };
+// myfun2();
